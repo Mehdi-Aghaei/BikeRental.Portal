443c70c968e341ab5c05b8e780ca643089299268
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2022-07-28 21:32:32 +0200

    CODE RUB: Add bootstrap

--- BikeRental.Portal.Web/Views/Components/Test.razor.cs
+++ BikeRental.Portal.Web/Views/Components/Test.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BikeRental.Portal.Web.Models.Bikes;
+using BikeRental.Portal.Web.Services.Foundations.Bikes;
+using Microsoft.AspNetCore.Components;
 
 namespace BikeRental.Portal.Web.Views.Components;
 
@@ -6,4 +8,24 @@ public partial class Test
 {
     [Parameter]
     public string Info { get; set; } = string.Empty;
+
+    public Bike Bike { get; set; } = new Bike();
+
+    [Inject]
+    public IBikeService bikeService { get; set; }
+
+    protected override Task OnInitializedAsync()
+    {
+
+        return base.OnInitializedAsync();
+    }
+    
+    protected async Task HandleValidSubmit()
+    {
+        
+    } 
+    protected async Task HandleInvalidSubmit()
+    {
+        
+    }
 }
